9ab2d4fa9cecadc988252da143462254f08ee68d
Felipe <felipejsilvacosta@gmail.com>
2020-10-16 18:40:00 +1100

    dish finished friday class

--- main.rb
+++ main.rb
@@ -3,41 +3,50 @@ require 'sinatra/reloader'
 require 'pg'
 require 'pry'
 
+def run_sql(sql)
+  db = PG.connect(dbname: 'goodfoodhunting')
+  db.close
+  return db.exec(sql)
+end
 
 get '/' do
-# connect to db
-
-db = PG.connect(dbname: 'goodfoodhunting')
-# send and sql to the db to execute
-# records to returned to us
-dishes = db.exec('SELECT * FROM dishes;')
-db.close
-# binding.pry
-
+  dishes = run_sql('SELECT * FROM dishes ORDER BY name;')
   erb :index, locals: { dishes: dishes }
 end
 
-get '/details' do
-  
-  db = PG.connect(dbname: 'goodfoodhunting')
-  results = db.exec("SELECT * FROM dishes WHERE id = #{params['id']};")
-  db.close
-  erb :details, locals: {dish: results[0]}
-end
-
-get '/form' do
+get '/dishes/new' do
   erb :new
 end
 
-post '/create' do
-  db = PG.connect(dbname: 'goodfoodhunting')
-
-  db.exec("INSERT INTO dishes (name, image_url) VALUES ('#{params['name']}','#{params['image_url']}');")
- db.close
-
- redirect "/"
+get '/dishes/:id' do # wildcat, can be anything when it s in :
+  results = run_sql("SELECT * FROM dishes WHERE id = #{params['id']};")
+  erb :details, locals: {dish: results[0]}
 end
 
-# http methods - they actually have meanings
+get '/dishes/:id/edit' do
+  results = run_sql("SELECT * FROM dishes WHERE id = #{params['id']};")
+  erb :edit, locals: {dish: results[0]}
+end
 
+post '/dishes' do
+  run_sql("INSERT INTO dishes (name, image_url) VALUES ('#{params['name']}','#{params['image_url']}');")
+  redirect "/"
+end
+
+patch '/dishes/:id' do
+  run_sql("UPDATE dishes SET name = '#{params['name']}', image_url = '#{params['image_url']}' WHERE id = #{params["id"]};")
+  redirect "/dishes/#{params["id"]}"
+end
+
+delete '/dishes/:id' do
+  run_sql("DELETE FROM dishes WHERE id = #{params['id']}")
+  redirect "/"
+end
+
+
+# http methods - they actually have meanings
 #a route is both the http method + path
+# get - just for reading
+# post - to create
+# patch - to change
+# delete - to delete
